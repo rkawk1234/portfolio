1ef7d6f4213b7b3245177f5f2e9e980ae0be5643
rkawk1234 <okus6977@gmail.com>
2022-10-23 14:29:51 -0400

    navbar updated
    10.23.2022.14:29

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold font-signature">Header</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+
+const NavBar = () => {
+  return (
+    <div className='fixed flex justify-between items-center w-full h-20 text-white bg-black'>
+
+    </div>
+
+  )
+}
+
+export default NavBar
