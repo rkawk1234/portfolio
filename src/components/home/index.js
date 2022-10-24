2084974c92aaff274cfc6c0d85bc07e765c8bc68
rkawk1234 <okus6977@gmail.com>
2022-10-24 10:24:17 -0400

    home updated
    10242022 10:24

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -15,7 +15,7 @@ const Home = () => {
     <div className='h-screen w-full bg-gradient-to-b from-black to-gray-500'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col items-center justify-center h-full'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white'> Web Developer</h2>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white'> My Personal Website</h2>
                 <p className='text-lg sm:text-xl text-gray-600 py-2'> With Javascript, React and Tailwind CSS </p>
                 <button className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-br from-gray-600 to-gray-400 cursor-pointer hover:animate-bounce'>
                     About Me
@@ -23,34 +23,12 @@ const Home = () => {
             </div>
 
             <div>
-            <div className="stage-cube-cont">
-                    <div className="cubespinner">
-                        <div className="face1">
-                        <FaJava/>
-                        </div>
-                        <div className="face2">
-                        <FaHtml5/>
-                        </div>
-                        <div className="face3">
-                        <FaCss3/>
-                        </div>
-                        <div className="face4">
-                        <FaReact/>
-                        </div>
-                        <div className="face5">
-                        <FaJsSquare/>
-                        </div>
-                        <div className="face6">
-                        <FaPython/>
-                        </div>
-                    </div>
-                </div>
-                {/* <img
+                <img
                 src={ReactTailwind}
                 alt="react"
                 className='rounded-2xl mx-auto w-2/3 md:w-2/3 hover:animate-pulse'>
 
-                </img> */}
+                </img>
                 
             </div>
 
