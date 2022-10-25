f61716cfb9af7fd8d4d0b7d8367089a485ffbbb2
rkawk1234 <okus6977@gmail.com>
2022-10-25 12:02:41 -0400

    contact form added
    auto indent complete

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,41 +1,41 @@
 import React from 'react'
 import { Link } from "react-scroll";
-import {FaArrowDown} from 'react-icons/fa'
+import { FaArrowDown } from 'react-icons/fa'
 import './index.css'
 
 
 const About = () => {
-  return (
-    <div id='about' className='w-full h-screen bg-gradient-to-b from-gray-400 to-gray-600 text-white'>
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 '>
+    return (
+        <div id='about' className='w-full h-screen bg-gradient-to-b from-gray-400 to-gray-600 text-white'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 '>
 
-        <div className='flex flex-col items-center justify-center h-full'>
+                <div className='flex flex-col items-center justify-center h-full'>
 
-            <div className='pt-8 pb-8'>
-                <p className='text-4xl inline animate-pulse'>About Me</p>
+                    <div className='pt-8 pb-8'>
+                        <p className='text-4xl inline animate-pulse'>About Me</p>
+                    </div>
+                    <p className='text-base'>
+                        Hi, my name is Min and I am a full-stack web developer. I have both front-end and back-end web development experiences with different languages and frameworks.
+                        For front-end webdevelopment, I use HTML,CSS,Javascript, and javascript library, React. For Back-end web development, I normally use Python and Django for the framework.
+                        I've graduated from University of Waterloo and I have a Bachelor's degree in Mathematics, major in computational mathematics, minor in computer science.
+                        Computational Mathematics is an intersection of mathematics, statistics, optimization, and computer science, to solve problems in various fields, which include business, finance, medicine, science, and technology.
+
+
+
+
+                    </p>
+                </div>
+
+
+                <div className='pb-3 hover:scale-125 text-black'>
+                    <Link to='portfolio' smooth duration={500}>
+                        <FaArrowDown size={50} />
+                    </Link>
+                </div>
+
             </div>
-            <p className='text-base'>
-                Hi, my name is Min and I am a full-stack web developer. I have both front-end and back-end web development experiences with different languages and frameworks. 
-                For front-end webdevelopment, I use HTML,CSS,Javascript, and javascript library, React. For Back-end web development, I normally use Python and Django for its framework.
-                I graduated from University of Waterloo and I have a Bachelor's degree in Mathematics, major in computational mathematics, minor in computer science.
-                Computational Mathematics is an intersection of mathematics, statistics, optimization, and computer science, to solve problems in business, finance, medicine, science, and technology.
-
-                
-                
-                
-            </p>
         </div>
-
-
-            <div className='pb-3 hover:scale-125 text-black'>
-                <Link to='portfolio' smooth duration={500}>
-                    <FaArrowDown size={50}/>
-                </Link>
-            </div>
-                
-        </div>
-    </div>
-  )
+    )
 }
 
 export default About
--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from "react-scroll";
-import {FaArrowDown} from 'react-icons/fa'
+import { FaArrowDown } from 'react-icons/fa'
 import './index.css'
 
 const Portfolio = () => {
-  return (
-    <div id='portfolio' className='w-full h-screen bg-gradient-to-b from-gray-400 to-gray-600 text-white'>
-    <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 '>
-{/* 
+    return (
+        <div id='portfolio' className='w-full h-screen bg-gradient-to-b from-gray-400 to-gray-600 text-white'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 '>
+                {/* 
         <div className='flex flex-col items-center justify-center h-full'>
 
             <div className='pt-8 pb-8'>
@@ -15,23 +15,23 @@ const Portfolio = () => {
             </div>
         </div> */}
 
-        <div className='grid grid-cols-2  text-center items-center justify-center w-full h-full'>
-            <div> First Project</div>
-            <div> Second Project</div>
-            <div> Third Project</div>
-            <div> Fourth Project</div>
+                <div className='grid grid-cols-2  text-center items-center justify-center w-full h-full'>
+                    <div> First Project</div>
+                    <div> Second Project</div>
+                    <div> Third Project</div>
+                    <div> Fourth Project</div>
 
-        </div>
+                </div>
 
-            <div className='pb-3 hover:scale-125 text-black'>
-                <Link to='contact' smooth duration={500}>
-                    <FaArrowDown size={50}/>
-                </Link>
+                <div className='pb-3 hover:scale-125 text-black'>
+                    <Link to='contact' smooth duration={500}>
+                        <FaArrowDown size={50} />
+                    </Link>
+                </div>
+
             </div>
-                
         </div>
-</div>
-  )
+    )
 }
 
 export default Portfolio
