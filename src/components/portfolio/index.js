88c97801fbdc5661a449847000f96ce81ef40a13
rkawk1234 <okus6977@gmail.com>
2022-11-03 04:21:43 -0400

    Update index.js

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -1,40 +1,68 @@
 import React from 'react'
 import portfolio1 from '../../assets/portfolio1.png'
 import portfolio2 from '../../assets/portfolio2.png'
+import portfolio3 from '../../assets/portfolio3.png'
+import portfolio4 from '../../assets/portfolio4.png'
+
 import './index.css'
 
 const Portfolio = () => {
-    const portfolios = [
-        {
-          id: 1,
-          src: portfolio1,
-        },
-        {
-          id: 2,
-          src: portfolio2,
-        },
+  const portfolios = [
+    {
+      id: 1,
+      src: portfolio1,
+    },
+    {
+      id: 2,
+      src: portfolio2,
+    },
+    {
+      id: 3,
+      src: portfolio3,
+    },
+    {
+      id: 4,
+      src: portfolio4,
+    },
 
 
-      ];
+  ];
 
-    return (
-        <div id='portfolio' className='w-full h-screen bg-gradient-to-b from-gray-600 to-gray-800 text-white'>
-            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 '>
-
-                <div className='grid grid-rows-2  w-2/3 h-full py-20'>
-                    <div className=''> <img className='rounded-lg' src={portfolio1} alt="Portfolio1"/></div>
-                    <div> <img className='rounded-lg' src={portfolio2} alt="Portfolio2"/></div>
-
-
-                </div>
-
-                
-
-
-            </div>
-
+  return (
+    <div id='portfolio' className='w-full h-screen bg-gradient-to-b from-gray-600 to-gray-800 text-white'>
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 '>
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline">
+            Projects
+          </p>
         </div>
-    )
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+
+
+      </div>
+
+    </div>
+  )
 }
 
 export default Portfolio
